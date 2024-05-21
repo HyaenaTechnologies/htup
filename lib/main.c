7459b476cedbeb6aefa7b8c273a57a601be4a9af
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-05-20 23:34:37 -0600

    Convert Main

--- lib/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("Local Workflow\n");
-    return 0;
-}
---
+++ lib/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Local Workflow\n";
+    return 0;
+}
