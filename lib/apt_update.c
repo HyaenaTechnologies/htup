02249858ffd64f29a37ab743715a096afe8cfd82
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-07-17 03:57:39 -0600

    Clean Up Code

--- lib/apt_update.c
+++ lib/apt_update.c
@@ -1,42 +1,44 @@
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+// Null Character Arrary Pointer
+const char null_characters = (char *)0;
+
 // Path Environment Variable
-const char *environment[2][14] = {
+const char *path_environment[2][14] = {
     "PATH=/usr/bin",
-    (const char *)0
+    null_characters
 };
 
 // Program Termination State
+
 const int fatal_error = 1;
 const int program_successful = 0;
 
 // APT Update
 int main(void) {
-    const int apt_update = fork();
-    const int fork_failed = -1;
-    const int fork_successful = 0;
+    int clone_process = fork();
+    const int clone_error = -1;
+    const int clone_successful = 0;
 
-    if (apt_update == fork_failed) {
+    if (clone_process == clone_error) {
         perror("APT Update Failed - fork() = -1: \n");
         exit(fatal_error);
     };
 
-    if (apt_update == fork_successful) {
+    if (clone_process == clone_successful) {
         execle(
             "/usr/bin/sh", 
             "sh", 
             "-c",
             "apt update && apt -y full-upgrade",
-            (char *)0, 
-            environment
+            null_characters, 
+            path_environment
         );
     } else {
-        waitpid(apt_update, NULL, 0);
+        waitpid(clone_process, NULL, 0);
     };
 
     exit(program_successful);
